1662a655bdc8a9704e47e2a23ea4e3b2ee41cbbf
Collins <mutaic84@gmail.com>
2022-05-05 11:48:22 +0300

    display comments with the user who posted

--- src/components/Comments.js
+++ src/components/Comments.js
@@ -1,9 +1,21 @@
 import React from "react";
 
-function Comments() {
+function Comments({comments}) {
+  console.log(comments)
+  const commentList = comments.map((comment) => {
+    return (
+      <>
+        <h4>{comment.user}</h4>
+        <p>{comment.comment}</p>
+      </>
+    )
+  })
   return (
     <div>
       <button>Comments show</button>
+      <hr />
+      <h3>{comments.length} Comments</h3>
+      {commentList}
     </div>
   )
 }
