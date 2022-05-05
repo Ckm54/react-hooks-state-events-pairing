927c3ca5462551c6a683285d382d5503d807be13
Collins <mutaic84@gmail.com>
2022-05-05 11:09:37 +0300

    display given video

--- src/components/App.js
+++ src/components/App.js
@@ -8,7 +8,7 @@ function App() {
       <iframe
         width="919"
         height="525"
-        src="https://www.youtube.com/embed/dQw4w9WgXcQ"
+        src={video.embedUrl}
         frameBorder="0"
         allowFullScreen
         title="Thinking in React"
