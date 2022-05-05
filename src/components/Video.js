43cdee250412c1deb0a4fd1b1f337c7956c1ffa6
Collins <mutaic84@gmail.com>
2022-05-05 12:15:51 +0300

    move comments component to video and update props

--- src/components/Video.js
+++ src/components/Video.js
@@ -1,6 +1,8 @@
 import React from "react";
+import Comments from "./Comments";
+import Votes from "./Votes";
 
-function Video({video, title, views, date}) {
+function Video({video}) {
   return (
     <>
     <iframe
@@ -11,8 +13,10 @@ function Video({video, title, views, date}) {
         allowFullScreen
         title="Thinking in React"
       />
-      <h2>{title}</h2>
-      <p>{views} Views | Uploaded {date}</p>
+      <h2>{video.title}</h2>
+      <p>{video.views} Views | Uploaded {video.createdAt}</p>
+      <Votes upvotes={video.upvotes} downvotes={video.downvotes}/>
+      <Comments comments={video.comments} />
     </>
   )
 }
